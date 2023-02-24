178545d07852314339730eb6fe908824e607b895
sungmin <kimmoksil@gmail.com>
2023-02-24 10:12:00 +0900

    init commit

---
+++ testBootPrj/src/main/java/com/newlecture/web/TestBootPrjApplication.java
@@ -0,0 +1,14 @@
+package com.newlecture.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cglib.core.AbstractClassGenerator;
+
+@SpringBootApplication
+public class TestBootPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBootPrjApplication.class, args);
+	}
+
+}
